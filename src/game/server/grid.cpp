298d8fb404d66285015f6e54d8a64c85521dcb93
Marlon Rondinelli <marlon@marlonrondinelli.com>
2025-07-16 15:07:45 -0700

    Remove `Wall`, add `Grid`

---
+++ src/game/server/grid.cpp
@@ -0,0 +1,79 @@
+#include "grid.hpp"
+
+namespace fpsparty::game {
+Grid::Grid(const Grid_create_info &create_info)
+    : _width_chunks{(create_info.width + (_chunk_side_length - 1)) /
+                    _chunk_side_length},
+      _height_chunks{(create_info.height + (_chunk_side_length - 1)) /
+                     _chunk_side_length},
+      _depth_chunks{(create_info.depth + (_chunk_side_length - 1)) /
+                    _chunk_side_length} {
+  _chunks.reserve(_width_chunks * _height_chunks * _depth_chunks);
+}
+
+bool Grid::is_solid(const Eigen::Vector3i &location) const noexcept {
+  if (!bounds_check(location)) {
+    return false;
+  }
+  const auto chunk_index = get_chunk_index(location);
+  const auto chunk_value = _chunks[chunk_index];
+  const auto chunk_relative_x = location.x() % _chunk_side_length;
+  const auto chunk_relative_y = location.y() % _chunk_side_length;
+  const auto chunk_relative_z = location.z() % _chunk_side_length;
+  const auto bit_index =
+      chunk_relative_x + chunk_relative_y * _chunk_side_length +
+      chunk_relative_z * _chunk_side_length * _chunk_side_length;
+  return (chunk_value & (std::uint64_t{1} << bit_index)) != 0;
+}
+
+void Grid::set_solid(const Eigen::Vector3i &location, bool value) noexcept {
+  if (!bounds_check(location)) {
+    return;
+  }
+  const auto chunk_index = get_chunk_index(location);
+  const auto chunk_relative_x = location.x() % _chunk_side_length;
+  const auto chunk_relative_y = location.y() % _chunk_side_length;
+  const auto chunk_relative_z = location.z() % _chunk_side_length;
+  const auto bit_index =
+      chunk_relative_x + chunk_relative_y * _chunk_side_length +
+      chunk_relative_z * _chunk_side_length * _chunk_side_length;
+  if (value) {
+    _chunks[chunk_index] |= std::uint64_t{1} << bit_index;
+  } else {
+    _chunks[chunk_index] &= ~(std::uint64_t{1} << bit_index);
+  }
+}
+
+bool Grid::bounds_check(const Eigen::Vector3i &location) const noexcept {
+  if (location.x() < 0) {
+    return false;
+  }
+  if (static_cast<std::size_t>(location.x()) >=
+      _width_chunks * _chunk_side_length) {
+    return false;
+  }
+  if (location.y() < 0) {
+    return false;
+  }
+  if (static_cast<std::size_t>(location.y()) >=
+      _height_chunks * _chunk_side_length) {
+    return false;
+  }
+  if (location.z() < 0) {
+    return false;
+  }
+  if (static_cast<std::size_t>(location.z()) >=
+      _depth_chunks * _chunk_side_length) {
+    return false;
+  }
+  return true;
+}
+
+std::size_t
+Grid::get_chunk_index(const Eigen::Vector3i &location) const noexcept {
+  const auto x = static_cast<std::size_t>(location.x());
+  const auto y = static_cast<std::size_t>(location.y());
+  const auto z = static_cast<std::size_t>(location.z());
+  return x + y * _width_chunks + z * _width_chunks * _height_chunks;
+}
+} // namespace fpsparty::game
---
+++ src/game/server/grid.hpp
@@ -0,0 +1,36 @@
+#ifndef FPSPARTY_GAME_GRID_HPP
+#define FPSPARTY_GAME_GRID_HPP
+
+#include <Eigen/Dense>
+#include <cstddef>
+
+namespace fpsparty::game {
+struct Grid_create_info {
+  std::size_t width{};
+  std::size_t height{};
+  std::size_t depth{};
+};
+
+class Grid {
+public:
+  explicit Grid(const Grid_create_info &create_info);
+
+  bool is_solid(const Eigen::Vector3i &location) const noexcept;
+
+  void set_solid(const Eigen::Vector3i &location, bool value) noexcept;
+
+  bool bounds_check(const Eigen::Vector3i &location) const noexcept;
+
+private:
+  static constexpr auto _chunk_side_length = std::size_t{4};
+
+  std::size_t get_chunk_index(const Eigen::Vector3i &location) const noexcept;
+
+  std::size_t _width_chunks{};
+  std::size_t _height_chunks{};
+  std::size_t _depth_chunks{};
+  std::vector<std::uint64_t> _chunks{};
+};
+} // namespace fpsparty::game
+
+#endif
