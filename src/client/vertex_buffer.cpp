020e1ea9232ffc7321ef12d1c6e8e529bba226fb
Marlon Rondinelli <marlon@pensieve.co>
2025-08-01 16:51:10 -0700

    Trailing commas

--- src/client/vertex_buffer.cpp
+++ src/client/vertex_buffer.cpp
@@ -28,21 +28,29 @@ Vertex_buffer::Vertex_buffer(vk::CommandPool command_pool,
               .priority = {},
           });
   const auto command_buffer = std::move(
-      Global_vulkan_state::get().device().allocateCommandBuffersUnique(
-          {.commandPool = command_pool,
-           .level = vk::CommandBufferLevel::ePrimary,
-           .commandBufferCount = 1})[0]);
-  command_buffer->begin(
-      {.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+      Global_vulkan_state::get().device().allocateCommandBuffersUnique({
+          .commandPool = command_pool,
+          .level = vk::CommandBufferLevel::ePrimary,
+          .commandBufferCount = 1,
+      })[0]);
+  command_buffer->begin({
+      .flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit,
+  });
   command_buffer->copyBuffer(
       staging_buffer.get_buffer(), *_buffer,
-      {{.srcOffset = 0,
-        .dstOffset = 0,
-        .size = static_cast<vk::DeviceSize>(data.size())}});
+      {{
+          .srcOffset = 0,
+          .dstOffset = 0,
+          .size = static_cast<vk::DeviceSize>(data.size()),
+      }});
   command_buffer->end();
   const auto fence = Global_vulkan_state::get().device().createFenceUnique({});
   Global_vulkan_state::get().queue().submit(
-      {{.commandBufferCount = 1, .pCommandBuffers = &*command_buffer}}, *fence);
+      {{
+          .commandBufferCount = 1,
+          .pCommandBuffers = &*command_buffer,
+      }},
+      *fence);
   std::ignore = Global_vulkan_state::get().device().waitForFences(
       {*fence}, vk::True, std::numeric_limits<std::uint64_t>::max());
 }
