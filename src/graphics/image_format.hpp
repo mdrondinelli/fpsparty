8b2f90128ef4aba20be83d8c1abef15787d0684d
Marlon Rondinelli <marlon@pensieve.co>
2025-08-28 17:00:59 -0700

    Add `get_image_format_vk_image_aspect_flags`

--- src/graphics/image_format.hpp
+++ src/graphics/image_format.hpp
@@ -8,6 +8,20 @@ enum class Image_format {
   b8g8r8a8_srgb = static_cast<int>(vk::Format::eB8G8R8A8Srgb),
   d32_sfloat = static_cast<int>(vk::Format::eD32Sfloat),
 };
+
+namespace detail {
+constexpr vk::ImageAspectFlags
+get_image_format_vk_image_aspect_flags(Image_format format) {
+  switch (format) {
+  case Image_format::b8g8r8a8_srgb:
+    return vk::ImageAspectFlagBits::eColor;
+  case Image_format::d32_sfloat:
+    return vk::ImageAspectFlagBits::eDepth;
+  default:
+    return {};
+  }
 }
+} // namespace detail
+} // namespace fpsparty::graphics
 
 #endif
