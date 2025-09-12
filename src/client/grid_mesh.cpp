f2eb13d5af2075f608df5ed6fd465c0c5b204345
Marlon Rondinelli <marlon@pensieve.co>
2025-09-12 16:49:24 -0700

    Implement grid mesh

---
+++ src/client/grid_mesh.cpp
@@ -0,0 +1,170 @@
+#include "grid_mesh.hpp"
+#include "client/grid_vertex.hpp"
+#include "serial/span_writer.hpp"
+#include <iostream>
+#include <vector>
+
+namespace fpsparty::client {
+namespace {
+constexpr auto cell_stride = 1.0f;
+constexpr auto wall_half_thickness = 1.0f / 16.0f;
+
+std::pair<std::array<Grid_vertex, 4>, std::array<std::uint32_t, 6>>
+generate_face(const Eigen::Vector3i &cell_indices,
+              const Eigen::Vector3f &u,
+              const Eigen::Vector3f &v,
+              std::uint32_t face_begin_index) {
+  const auto cell_position = (cell_indices.cast<float>() * cell_stride).eval();
+  auto vertices = std::array<Grid_vertex, 4>{
+      Grid_vertex{
+          .position =
+              cell_position + u * wall_half_thickness + v * wall_half_thickness,
+      },
+      Grid_vertex{
+          .position = cell_position + u * (cell_stride - wall_half_thickness) +
+                      v * wall_half_thickness,
+      },
+      Grid_vertex{
+          .position = cell_position + u * (cell_stride - wall_half_thickness) +
+                      v * (cell_stride - wall_half_thickness),
+      },
+      Grid_vertex{
+          .position = cell_position + u * wall_half_thickness +
+                      v * (cell_stride - wall_half_thickness),
+      },
+  };
+  auto indices = std::array<std::uint32_t, 6>{
+      face_begin_index + 0,
+      face_begin_index + 1,
+      face_begin_index + 2,
+      face_begin_index + 2,
+      face_begin_index + 3,
+      face_begin_index + 0,
+  };
+  return {vertices, indices};
+}
+} // namespace
+
+Grid_mesh::Grid_mesh(const Grid_mesh_create_info &info) {
+  auto x_vertices = std::vector<Grid_vertex>{};
+  auto y_vertices = std::vector<Grid_vertex>{};
+  auto z_vertices = std::vector<Grid_vertex>{};
+  auto x_indices = std::vector<std::uint32_t>{};
+  auto y_indices = std::vector<std::uint32_t>{};
+  auto z_indices = std::vector<std::uint32_t>{};
+  constexpr auto n = static_cast<int>(game::Chunk::edge_length);
+  for (const auto &[chunk_indices, chunk] : info.grid->get_chunks()) {
+    for (auto x = 0; x != n; ++x) {
+      for (auto y = 0; y != n; ++y) {
+        for (auto z = 0; z != n; ++z) {
+          const auto cell_indices = Eigen::Vector3i{
+              chunk_indices.x() * n + x,
+              chunk_indices.y() * n + y,
+              chunk_indices.z() * n + z,
+          };
+          if (chunk->is_solid(game::Axis::x, {x, y, z})) {
+            const auto [face_vertices, face_indices] =
+                generate_face(cell_indices,
+                              {0.0f, 1.0f, 0.0f},
+                              {0.0f, 0.0f, 1.0f},
+                              static_cast<std::uint32_t>(x_vertices.size()));
+            x_vertices.append_range(face_vertices);
+            x_indices.append_range(face_indices);
+          }
+          if (chunk->is_solid(game::Axis::y, {x, y, z})) {
+            const auto [face_vertices, face_indices] =
+                generate_face(cell_indices,
+                              {0.0f, 0.0f, 1.0f},
+                              {1.0f, 0.0f, 0.0f},
+                              static_cast<std::uint32_t>(y_vertices.size()));
+            y_vertices.append_range(face_vertices);
+            y_indices.append_range(face_indices);
+          }
+          if (chunk->is_solid(game::Axis::z, {x, y, z})) {
+            const auto [face_vertices, face_indices] =
+                generate_face(cell_indices,
+                              {1.0f, 0.0f, 0.0f},
+                              {0.0f, 1.0f, 0.0f},
+                              static_cast<std::uint32_t>(z_vertices.size()));
+            z_vertices.append_range(face_vertices);
+            z_indices.append_range(face_indices);
+          }
+        }
+      }
+    }
+  }
+  const auto vertex_buffer_size =
+      (x_vertices.size() + y_vertices.size() + z_vertices.size()) *
+      sizeof(Grid_vertex);
+  const auto index_buffer_size =
+      (x_indices.size() + y_indices.size() + z_indices.size()) *
+      sizeof(std::uint32_t);
+  if (vertex_buffer_size > 0 && index_buffer_size > 0) {
+    auto staging_data = std::vector<std::byte>{};
+    staging_data.resize(vertex_buffer_size + index_buffer_size);
+    auto staging_data_writer = serial::Span_writer{staging_data};
+    staging_data_writer.write(std::as_bytes(std::span{x_vertices}));
+    staging_data_writer.write(std::as_bytes(std::span{y_vertices}));
+    staging_data_writer.write(std::as_bytes(std::span{z_vertices}));
+    staging_data_writer.write(std::as_bytes(std::span{x_indices}));
+    staging_data_writer.write(std::as_bytes(std::span{y_indices}));
+    staging_data_writer.write(std::as_bytes(std::span{z_indices}));
+    const auto staging_buffer =
+        info.graphics->create_staging_buffer(staging_data);
+    _vertex_buffer = info.graphics->create_vertex_buffer(vertex_buffer_size);
+    _index_buffer = info.graphics->create_index_buffer(index_buffer_size);
+    auto recorder = info.graphics->record_transient_work();
+    recorder.copy_buffer(staging_buffer,
+                         _vertex_buffer,
+                         {
+                             .src_offset = 0,
+                             .dst_offset = 0,
+                             .size = vertex_buffer_size,
+                         });
+    recorder.copy_buffer(staging_buffer,
+                         _index_buffer,
+                         {
+                             .src_offset = vertex_buffer_size,
+                             .dst_offset = 0,
+                             .size = index_buffer_size,
+                         });
+    _upload_work = info.graphics->submit_transient_work(std::move(recorder));
+    _upload_work->add_done_callback(this);
+    _draw_infos[0].index_count = x_indices.size();
+    _draw_infos[1].index_count = y_indices.size();
+    _draw_infos[2].index_count = z_indices.size();
+    _draw_infos[1].first_index = x_indices.size();
+    _draw_infos[2].first_index = x_indices.size() + y_indices.size();
+    _draw_infos[1].vertex_offset = x_vertices.size();
+    _draw_infos[2].vertex_offset = x_vertices.size() + y_vertices.size();
+  }
+}
+
+Grid_mesh::~Grid_mesh() {
+  if (_upload_work) {
+    _upload_work->remove_done_callback(this);
+  }
+}
+
+void Grid_mesh::record_draw_command(graphics::Work_recorder &recorder,
+                                    game::Axis axis) {
+  assert(is_uploaded());
+  recorder.draw_indexed(_draw_infos[static_cast<int>(axis)]);
+}
+
+bool Grid_mesh::is_uploaded() const noexcept {
+  return _vertex_buffer && _index_buffer && !_upload_work;
+}
+
+const rc::Strong<graphics::Vertex_buffer> &
+Grid_mesh::get_vertex_buffer() const noexcept {
+  return _vertex_buffer;
+}
+
+const rc::Strong<graphics::Index_buffer> &
+Grid_mesh::get_index_buffer() const noexcept {
+  return _index_buffer;
+}
+
+void Grid_mesh::on_work_done(const graphics::Work &) { _upload_work = nullptr; }
+} // namespace fpsparty::client
--- src/client/grid_mesh.hpp
+++ src/client/grid_mesh.hpp
@@ -2,19 +2,40 @@
 #define FPSPARTY_CLIENT_GRID_MESH_HPP
 
 #include "game/core/grid.hpp"
-#include "graphics/staging_buffer.hpp"
+#include "graphics/graphics.hpp"
+#include "graphics/index_buffer.hpp"
 #include "graphics/vertex_buffer.hpp"
+#include "graphics/work_done_callback.hpp"
+#include "graphics/work_recorder.hpp"
 #include "rc.hpp"
-#include <utility>
 
 namespace fpsparty::client {
-class Grid_mesh {
-public:
-  void update(const game::Grid &grid);
+struct Grid_mesh_create_info {
+  graphics::Graphics *graphics;
+  const game::Grid *grid;
+};
 
-  std::pair<rc::Strong<graphics::Vertex_buffer>,
-            rc::Strong<graphics::Staging_buffer>>
-  get_current_buffers() const;
+class Grid_mesh : graphics::Work_done_callback {
+public:
+  explicit Grid_mesh(const Grid_mesh_create_info &info);
+
+  ~Grid_mesh();
+
+  void record_draw_command(graphics::Work_recorder &recorder, game::Axis axis);
+
+  bool is_uploaded() const noexcept;
+
+  const rc::Strong<graphics::Vertex_buffer> &get_vertex_buffer() const noexcept;
+
+  const rc::Strong<graphics::Index_buffer> &get_index_buffer() const noexcept;
+
+private:
+  void on_work_done(const graphics::Work &work) override;
+
+  rc::Strong<graphics::Vertex_buffer> _vertex_buffer{};
+  rc::Strong<graphics::Index_buffer> _index_buffer{};
+  rc::Strong<graphics::Work> _upload_work{};
+  std::array<graphics::Indexed_draw_info, 3> _draw_infos;
 };
 } // namespace fpsparty::client
 
