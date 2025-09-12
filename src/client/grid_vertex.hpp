eaa87535ca09d2833d42d16f9f050099dd0bf46f
Marlon Rondinelli <marlon@pensieve.co>
2025-09-12 16:35:12 -0700

    Define grid mesh vertex format

---
+++ src/client/grid_vertex.hpp
@@ -0,0 +1,12 @@
+#ifndef FPSPARTY_CLIENT_GRID_VERTEX_HPP
+#define FPSPARTY_CLIENT_GRID_VERTEX_HPP
+
+#include <Eigen/Dense>
+
+namespace fpsparty::client {
+struct Grid_vertex {
+  Eigen::Vector3f position;
+};
+} // namespace fpsparty::client
+
+#endif
